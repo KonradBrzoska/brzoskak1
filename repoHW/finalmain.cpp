ff787e4503524d7fa5e6d5d233e1dd459c9eda7e
KonradBrzoska <42821113+KonradBrzoska@users.noreply.github.com>
2018-12-19 17:01:22 -0500

    Add files via upload

---
+++ repoHW/finalmain.cpp
@@ -0,0 +1,155 @@
+#include <iostream>
+#include "tinyxml2.h"
+using namespace std;
+using namespace tinyxml2;
+
+
+int main()
+{
+  // using tinyxml2 library
+  XMLDocument doc;
+<RAlookup>
+  <RA>
+            <name first="First_Name" last="Last_Name">Nikki Maher<name first="First_Name" last="Last_Name"> // element is name, attribute are first and last, attribute values are in quotes
+            <hall>Crispell</hall>
+            <room>213</room>
+            <ext>5257</ext>
+  </RA>
+
+  <RA>
+          <name first="First_Name" last="Last_Name">Konrad Brzoska<name first="First_Name" last="Last_Name"> // element is name, attribute are first and last, attribute values are in quotes
+          <hall>Ridgeview</hall>
+          <room>212</room>
+          <ext>4413</ext>
+  </RA>
+
+  <RA>
+          <name first="First_Name" last="Last_Name">Edenilson Arias<name first="First_Name" last="Last_Name"> // element is name, attribute are first and last, attribute values are in quotes
+          <hall>Esopus</hall>
+          <room>040</room>
+          <ext>4016</ext>
+  </RA>
+
+  <RA>
+            <name first="First_Name" last="Last_Name">Jake Wasserman<name first="First_Name" last="Last_Name"> // element is name, attribute are first and last, attribute values are in quotes
+            <hall>Crispell</hall>
+            <room>302</room>
+            <ext>5180</ext>
+  </RA>
+
+  <RA>
+            <name first="First_Name" last="Last_Name">Jennifer Covais<name first="First_Name" last="Last_Name"> // element is name, attribute are first and last, attribute values are in quotes
+            <hall>Crispell</hall>
+            <room>211</room>
+            <ext>5275</ext>
+  </RA>
+
+  <RA>
+            <name first="First_Name" last="Last_Name">Nichole Santa<name first="First_Name" last="Last_Name"> // element is name, attribute are first and last, attribute values are in quotes
+            <hall>Crispell</hall>
+            <room>102</room>
+            <ext>5270</ext>
+  </RA>
+
+  <RA>
+            <name first="First_Name" last="Last_Name">Nikki Maher<name first="First_Name" last="Last_Name"> // element is name, attribute are first and last, attribute values are in quotes
+            <hall>Crispell</hall>
+            <room>213</room>
+            <ext>5257</ext>
+  </RA>
+
+  <RA>
+            <name first="First_Name" last="Last_Name">Dillon Mahbier<name first="First_Name" last="Last_Name"> // element is name, attribute are first and last, attribute values are in quotes
+            <hall>Esopus</hall>
+            <room>030</room>
+            <ext>4061</ext>
+  </RA>
+
+  <RA>
+            <name first="First_Name" last="Last_Name">Grant Sheshena<name first="First_Name" last="Last_Name"> // element is name, attribute are first and last, attribute values are in quotes
+            <hall>Esopus</hall>
+            <room>053</room>
+            <ext>4011</ext>
+  </RA>
+
+  <RA>
+            <name first="First_Name" last="Last_Name"> Helen Zheng<name first="First_Name" last="Last_Name"> // element is name, attribute are first and last, attribute values are in quotes
+            <hall>Esopus</hall>
+            <room>218</room>
+            <ext>4124</ext>
+  </RA>
+
+  <RA>
+            <name first="First_Name" last="Last_Name">Samual Halligan<name first="First_Name" last="Last_Name"> // element is name, attribute are first and last, attribute values are in quotes
+            <hall>Esopus</hall>
+            <room>339</room>
+            <ext>5391</ext>
+  </RA>
+
+  <RA>
+            <name first="First_Name" last="Last_Name">Eva Fontanze<name first="First_Name" last="Last_Name"> // element is name, attribute are first and last, attribute values are in quotes
+            <hall>Ridgeview</hall>
+            <room>117</room>
+            <ext>5350</ext>
+  </RA>
+
+  <RA>
+            <name first="First_Name" last="Last_Name">Dylan Hershal<name first="First_Name" last="Last_Name"> // element is name, attribute are first and last, attribute values are in quotes
+            <hall>Ridgeview</hall>
+            <room>408</room>
+            <ext>4323</ext>
+  </RA>
+
+  <RA>
+            <name first="First_Name" last="Last_Name">Jose Hermandez<name first="First_Name" last="Last_Name"> // element is name, attribute are first and last, attribute values are in quotes
+            <hall>Ridgeview</hall>
+            <room>303</room>
+            <ext>5406</ext>
+  </RA>
+
+  <RA>
+            <name first="First_Name" last="Last_Name">Kerri Herny<name first="First_Name" last="Last_Name"> // element is name, attribute are first and last, attribute values are in quotes
+            <hall>Ridgeview</hall>
+            <room>206</room>
+            <ext>5234</ext>
+  </RA>
+
+  <RA>
+            <name first="First_Name" last="Last_Name">Ishmeal Brown<name first="First_Name" last="Last_Name"> // element is name, attribute are first and last, attribute values are in quotes
+            <hall>Gage</hall>
+            <room>207</room>
+            <ext>2134</ext>
+  </RA>
+
+  <RA>
+            <name first="First_Name" last="Last_Name">Diara Paesto<name first="First_Name" last="Last_Name"> // element is name, attribute are first and last, attribute values are in quotes
+            <hall>Gage</hall>
+            <room>126</room>
+            <ext>4397</ext>
+  </RA>
+
+  <RA>
+            <name first="First_Name" last="Last_Name">Trent Reind<name first="First_Name" last="Last_Name"> // element is name, attribute are first and last, attribute values are in quotes
+            <hall>Gage</hall>
+            <room>345</room>
+            <ext>5864</ext>
+  </RA>
+
+  <RA>
+            <name first="First_Name" last="Last_Name">Amanda Kame<name first="First_Name" last="Last_Name"> // element is name, attribute are first and last, attribute values are in quotes
+            <hall>Gage</hall>
+            <room>211</room>
+            <ext>4739</ext>
+  </RA>
+
+  <RA>
+            <name first="First_Name" last="Last_Name">Caitlyn Buff<name first="First_Name" last="Last_Name"> // element is name, attribute are first and last, attribute values are in quotes
+            <hall>Gage</hall>
+            <room>141</room>
+            <ext>3075</ext>
+  </RA>
+
+
+  </RAlookup> //end of info
+  return 0;
+}
